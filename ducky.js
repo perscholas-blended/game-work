6955fc0e674202c52a7e90dbfbd8b793fa06858a
Charlton_Rosero <charltonprosero@gmail.com>
2022-02-06 20:57:41 -0500

    Fixed flapping

--- ducky.js
+++ ducky.js
@@ -15,7 +15,7 @@ window.onload = function () {
   // 2. Next, use setInterval to toggle the "flap" class on the duck every 250 ms (1/4 second)
   // https://www.w3schools.com/jsref/met_win_setinterval.asp
 function flapAnim(){
-  let flap = div.classList.toggle('.flap')  ///AHHHHH!!! IT's NOT WORKING!!!!!!!
+  let flap = div.classList.toggle('flap')  //OMG THE PROBLEM WAS A FREAKING PERIOD!!!
 }
 
    setInterval(flapAnim,230) 
@@ -68,9 +68,9 @@ function moveDuck(duck){
     div.className = 'duck'
     document.body.appendChild(div);   // It's working.
     function flapAnim(){
-      let flap = div.classList.toggle('.flap')
+      let flap = div.classList.toggle('flap')
     }
-       setInterval(flapAnim,220);    
+       setInterval(flapAnim,250);    
   }
     for(let i = 0; i < 4; i++){
         createDuck()
@@ -99,9 +99,22 @@ function moveDuck(duck){
   // 11. BOOM. Attach a "click" handler that adds the "shot" class to
   //     the duck when you click on it!
  
+function deadDuck(item){
+    let removeBloodyDucks = ()=>{
+      item.parentNode.removeChild(item);
+    } //will remove duck bodies
+
+    let duckDiv = item.className;
+      if(duckDiv.includes('shot')){
+        setTimeout(removeBloodyDucks, 100)
+      }// responsible for checking the condition.
+
+        // P.S.  this is #12
+
+  }
 
    elements = document.querySelectorAll(".duck");
-  // it is basically what i did in #3
+  // this part is basically what I did in #3
    for (let i = 0; i < elements.length; i++) { 
      // OMG IT WORKED?!?! I mean of course it worked.
       elements[i].addEventListener('click',boomBoom) // it does magic. jk. adds evenlistener for each item in the class duck
@@ -110,21 +123,26 @@ function moveDuck(duck){
           deadDuck(elements[i])
            setTimeout(chickenDinner,500)
 
-        }
-
+        } 
     }
   
   // 12. After a duck has been clicked on, remove it from the DOM after
   //     a short delay (1 second) Hint Hint...use setTimeout
   //     as for removing the element check out https://dzone.com/articles/removing-element-plain
   
+    //above #11
 
 
 
 
   // 13. Create a new function named checkForWinner() that reads the DOM
   //     to see if there are any ducks left. (How can we check the DOM for more than one element?, and how can we see how many elements we get back) If not, alert "YOU WIN!"
-
+  function chickenDinner(){
+    let winnerWinner = document.querySelectorAll('.duck');
+    if(winnerWinner.length == 0){
+     window.alert(" winner winner chicken dinner!! please don't tell nintendo about us. ") 
+    }   
+ }
 
   // 14. BONUS: The ducks are moving pretty erratically, can you think
   //     of a way to adjust the ducks speed based on how far needs to move?
